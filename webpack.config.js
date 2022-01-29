07af53ab30128d7e4644a4a6473fa6158440807a
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-01-29 04:41:01 -0300

    publicPath webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 const path = require('path');
-module.exports = {
-  entry: {
-    'dist/main': './src/main.tsx'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname)
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      { test: /\.js$/, use: ["source-map-loader"], enforce: "pre" }
-    ]
-  },
-  devServer: {
-    open: true,
-    static: path.join(__dirname)
-  },
-  devtool: 'source-map'
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      'dist/main': './src/main.tsx'
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname),
+      publicPath: argv.mode === 'production' ? '/apprun-todo/' : '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+      rules: [
+        { test: /\.tsx?$/, loader: 'ts-loader' },
+        { test: /\.js$/, use: ["source-map-loader"], enforce: "pre" }
+      ]
+    },
+    devServer: {
+      open: true,
+      static: path.join(__dirname)
+    },
+    devtool: 'source-map'
+  }
 }
